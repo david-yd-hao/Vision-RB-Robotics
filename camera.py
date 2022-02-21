a4dc861a2ffe8157d68d7524ffe2c3daae38333c
ShuohanT <53272243+114514sht@users.noreply.github.com>
2022-02-21 11:42:56 +0000

    camera

---
+++ camera.py
@@ -0,0 +1,47 @@
+import cv2
+import threading
+_capture = cv2.VideoCapture("http://localhost:8081/stream/video.mjpeg")
+#_capture = cv2.VideoCapture("outpy-1603900784.avi")
+#_capture = cv2.VideoCapture("output_1030.avi")
+_capture.set(cv2.CAP_PROP_BUFFERSIZE, 0)
+
+#out = cv2.VideoWriter('output_1030.avi', cv2.VideoWriter_fourcc(*"MJPG"), 20.0, (1016,760))
+
+if _capture.isOpened(): 
+
+    _rval, _frame  = _capture.read()
+else:
+    _rval = False
+_grey = None
+def _reader_func():
+    global _frame, _grey
+    while True:
+        import time
+        _rval, _f = _capture.read()
+        if _rval:
+            _frame = _f
+            _grey = cv2.cvtColor(_f, cv2.COLOR_BGR2GRAY)
+_reader = threading.Thread(target=_reader_func)
+_reader.daemon = True
+_reader.start()
+
+def get_frame(copy_=True):
+    global _frame
+    if copy_: return _frame.copy()
+    else: return _frame
+
+def get_grey(copy_=True):
+    global _grey
+    if copy_ and _grey is not None: return _grey.copy()
+    else: return _grey
+
+if __name__ == "__main__":
+    while(True):
+        if cv2.waitKey(1) == 27:
+            break
+        if cv2.waitKey(1) == 67:
+            cv2.imwrite('image1.png', _frame)
+        cv2.imshow("frame", get_frame())
+        #out.write(get_frame())
+    _capture.release()
+    out.release()
---
+++ test.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+import os
+import glob
+def calibrate():
+    CHECKERBOARD = (6,9)
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+    calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
+    objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
+    objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+    _img_shape = None
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('*.png')
+    images = [cv2.resize(cv2.imread(i), cv2.imread(images[0]).shape[:2][::-1]) for i in images]
+    for fname in images:
+        img = fname
+        if _img_shape == None:
+            _img_shape = img.shape[:2]
+        else:
+            assert _img_shape == img.shape[:2], "All images must share the same size."
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+            imgpoints.append(corners)
+    N_OK = len(objpoints)
+    K = np.zeros((3, 3))
+    D = np.zeros((4, 1))
+    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    rms, _, _, _, _ = \
+        cv2.fisheye.calibrate(
+            objpoints,
+            imgpoints,
+            gray.shape[::-1],
+            K,
+            D,
+            rvecs,
+            tvecs,
+            calibration_flags,
+            (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
+        )
+    print("Found " + str(N_OK) + " valid images for calibration")
+    print("DIM=" + str(_img_shape[::-1]))
+    print("K=np.array(" + str(K.tolist()) + ")")
+    print("D=np.array(" + str(D.tolist()) + ")")
+    DIM=_img_shape[::-1]
+    K=np.array(K)
+    D=np.array(D)
+    return K, D, DIM
+
+def undistort(img, K, D, DIM):
+    h,w = img.shape[:2]
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    return undistorted_img
+
+K, D, DIM = calibrate()
+img = undistort(cv2.resize(cv2.imread('val.png'), cv2.imread('img1.png').shape[:2][::-1]), K, D, DIM)
+cv2.imwrite('val2.png', img)
+
