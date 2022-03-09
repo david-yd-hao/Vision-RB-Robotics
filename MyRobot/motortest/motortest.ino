76a2f889fa80535c9ebb281ff98147ca29ba4439
David Hao <hydhyd2002@vip.sina.com>
2022-03-09 23:31:25 +0000

    Update motortest.ino

--- MyRobot/motortest/motortest.ino
+++ MyRobot/motortest/motortest.ino
@@ -9,51 +9,34 @@ For use with the Adafruit Motor Shield v2
 
 #include <Adafruit_MotorShield.h>
 
-// Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(4);
-// You can also make another motor on port M2
+Adafruit_DCMotor *LMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *RMotor = AFMS.getMotor(4);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
-
   if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
   // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
     Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
   }
   Serial.println("Motor Shield found.");
-
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
-  myOtherMotor->setSpeed(150);
-  myOtherMotor->run(FORWARD);
-  // turn on motor
-  myOtherMotor->run(RELEASE);
 }
 
 void loop() {
-  uint8_t i;
-
-  Serial.print("tick");
-
-  myMotor->run(FORWARD);
-  myOtherMotor->run(FORWARD);
-  myMotor->setSpeed(150);
-  myOtherMotor->setSpeed(150);
   delay(10000);
-  myMotor->setSpeed(0);
-  myOtherMotor->setSpeed(0);
-  myMotor->run(RELEASE);
-  myOtherMotor->run(RELEASE);
-  delay(3000);
+  LMotor->run(FORWARD);
+  RMotor->run(FORWARD);
+  LMotor->setSpeed(150);
+  RMotor->setSpeed(150);
+  delay(10000);
+  LMotor->setSpeed(50);
+  RMotor->setSpeed(50);
+  delay(5000);
+  LMotor->setSpeed(0);
+  RMotor->setSpeed(0);
+  LMotor->run(RELEASE);
+  RMotor->run(RELEASE);
+  delay(5000);
+
 }
